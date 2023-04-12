02fe59cb3569a50c853148c543cab4384372a500
Admin <Admin@DESKTOP-7O4IF1R>
2023-04-12 16:00:17 +0800

    yanaaa

--- src/ner/maama.java
+++ src/ner/maama.java
@@ -4,7 +4,7 @@ public class maama {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dasdbambam");
+		System.out.println("dasdbambamhhh");
 	}
 
 }
